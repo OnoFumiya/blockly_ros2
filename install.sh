3b2233577ddacbe616861a79bb9a80001a5f9a43
OnoFumiya <fumiyaono.choi@gmail.com>
2026-05-30 23:51:48 +0900

    Update firefox installation

--- install.sh
+++ install.sh
@@ -9,8 +9,23 @@ ENV="$(uname -m)"
 # Install for Each Environment
 if [[ ${ENV} == *"x86_64"* ]]; then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ./google-chrome-stable_current_amd64.deb
+    sudo apt install -y ./google-chrome-stable_current_amd64.deb
     sudo rm google-chrome-stable_current_amd64.deb
+
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- \
+| sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show \
+/etc/apt/keyrings/packages.mozilla.org.asc
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] \
+https://packages.mozilla.org/apt mozilla main" \
+| sudo tee /etc/apt/sources.list.d/mozilla.list
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
 else
     sudo rm -f /etc/apt/preferences.d/chromium-deb
     sudo tee /etc/apt/preferences.d/chromium-deb > /dev/null <<- 'EOF'
@@ -24,14 +39,13 @@ Pin-Priority: -1
 EOF
     sudo add-apt-repository -y ppa:xtradeb/apps
     sudo apt update
-    sudo apt install -y \
-        chromium \
-        firefox
+    sudo apt install -y chromium
 fi
 
 sudo apt update
 sudo apt install -y \
-    xdg-utils
+    xdg-utils \
+    firefox
 
 pip3 install qrcode[pil] --break-system-packages
 python3 -m pip install --break-system-packages \
