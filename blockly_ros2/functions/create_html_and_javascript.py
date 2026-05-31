fb6fe0bf4a2bab5352e8d1061059aea712d5302b
OnoFumiya <fumiyaono.choi@gmail.com>
2026-05-31 13:10:03 +0900

    get curent directory functions

--- blockly_ros2/functions/create_html_and_javascript.py
+++ blockly_ros2/functions/create_html_and_javascript.py
@@ -2,6 +2,7 @@ import os
 
 
 def get_directory_path():
-    dir_path = "/".join(__file__.split("/")[:-2])
-    base_dir_path = dir_path + "/template_files"
-    create_dir_path = dir_path + "/static"
+    base_dir_path = "/".join(__file__.split("/")[:-3]) + "/template_files"
+    create_dir_path = "/".join(__file__.split("/")[:-2]) + "/static"
+    print("\033[32m", base_dir_path, "\033[0m")
+    print("\033[32m", create_dir_path, "\033[0m")
