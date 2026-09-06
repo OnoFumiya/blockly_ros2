d8106ab7f4c0970df65866dc87e3cb22f4570260
OnoFumiya <fumiyaono.choi@gmail.com>
2026-09-07 01:42:17 +0900

    Add List mode from jazzy

--- blockly_ros2/functions/create_ros_msg.py
+++ blockly_ros2/functions/create_ros_msg.py
@@ -26,56 +26,116 @@ def create_interface(module_command, mode="msg"):
         return None
 
 
+def get_value(data, mem, msg_type):
+    # 最後msg_typeを見て代入
+    msg_type = re.sub(r"\d", "", msg_type.lower())
+
+    if ((msg_type == "int") or (msg_type == "integer")):
+        value = int(data.get(mem, 0))
+    elif ((msg_type == "float") or (msg_type == "double")):
+        value = float(data.get(mem, 0.0))
+    elif ((msg_type == "string") or (msg_type == "str") or (msg_type == "char")):
+        value = str(data.get(mem, ""))
+    elif ((msg_type == "bool") or (msg_type == "boolean")):
+        val = data.get(mem, True)
+        if ((val == "TRUE") or (val == "True") or (val == "true")):
+            value = True
+        else:
+            value = False
+    elif ((msg_type == "time")):
+        from builtin_interfaces.msg import Duration
+        val = float(data.get(mem, 0.0))
+        frac, integral = math.modf(val)
+        sec = int(integral)
+        nanosec = round(frac * 1_000_000_000)
+        if nanosec >= 1_000_000_000:
+            sec += 1
+            nanosec -= 1_000_000_000
+        value = Duration(sec=sec, nanosec=nanosec)
+    elif ((msg_type == "deg") or (msg_type == "degree")):
+        value = float(data.get(mem, 0.0)) * math.pi / 180.0
+    else:
+        value = None
+    
+    return value
+
+
+def replace_key(key):
+    replaced_key = ""
+    split_key = key.split("__")
+
+    num_input = True
+    for k in split_key:
+        if (k.isnumeric()):
+            replaced_key += "__" + k + "__"
+            num_input = True
+        else:
+            if num_input:
+                num_input = False
+            else:
+                replaced_key += "."
+            replaced_key += k
+
+    return replaced_key
+
+
 def set_members(data, msg, members, topic_name):
+
+    end_flag = False
+    temp_data = data.copy()
+    data = {}
+    for key, value in temp_data.items():
+        replaced_key = replace_key(key.replace(topic_name.replace("/", "") + "___", ""))
+        data[replaced_key] = value
+
+    while (not end_flag):
+        end_flag = True
+        replaced_members = {}
+        for mem, msg_type in members.items():
+            if (("[" in mem) and ("]" in mem)):
+                num = int((mem.split("[")[1]).split("]")[0])
+
+                for i in range(num):
+                    replaced_members[mem.split("[" + str(num))[0] + "__" + str(i) + "__" + ("[" + str(num)).join(mem.split("[" + str(num))[1:])[1:]] = msg_type
+                end_flag = False
+
+            else:
+                replaced_members[mem] = msg_type
+        members = replaced_members
+
+
     for mem, msg_type in members.items():
 
-        # "." または "/" で分割
-        mem_dict = re.split(r"[./]", mem)
-
+        value = get_value(data, mem, msg_type)
         target = msg
 
-        # 最後の1つ前まで辿る
-        for attr in mem_dict[:-1]:
-            target = getattr(target, attr)
+        # "." で分割
+        mem_dict = mem.split(".")
 
-        # 最後msg_typeを見て代入
-        msg_type = re.sub(r"\d", "", msg_type.lower())
-        get_namespace = topic_name.replace("/", "") + "___" + mem.replace(".", "__")
-
-        value = None
-        if ((msg_type == "int") or (msg_type == "integer")):
-            value = int(data.get(get_namespace, 0))
-        elif ((msg_type == "float") or (msg_type == "double")):
-            value = float(data.get(get_namespace, 0.0))
-        elif ((msg_type == "string") or (msg_type == "str") or (msg_type == "char")):
-            value = str(data.get(get_namespace, ""))
-        elif ((msg_type == "bool") or (msg_type == "boolean")):
-            val = data.get(get_namespace, True)
-            if ((val == "TRUE") or (val == "True") or (val == "true")):
-                value = True
+        for i in range(len(mem_dict)):
+            attr = mem_dict[i]
+            if ("__" in attr):
+                num = int(attr.split("__")[1])
+                temp_attr = attr.replace("__" + str(num) + "__", "")
+                temp_value = getattr(target, temp_attr)
+                if (i == len(mem_dict) - 1):
+                    add_msg = type(value)()
+                    if (len(temp_value) <= num):
+                        temp_value.extend([add_msg] * (num - len(temp_value) + 1))
+                        temp_value[num] = value
+                else:
+                    add_msg_type = str(target.__class__().get_fields_and_field_types()[temp_attr]).replace("sequence<", "").replace(">", "")
+                    module = importlib.import_module(add_msg_type.split("/")[0] + ".msg")
+                    add_msg = getattr(module, add_msg_type.split("/")[1])()
+                    if (len(temp_value.__class__()) <= num):
+                        temp_value.extend([add_msg] * (num - len(temp_value) + 1))
+                if (i == len(mem_dict) - 1):
+                    setattr(target, temp_attr, temp_value)
+                target = temp_value[num]
             else:
-                value = False
-        elif ((msg_type == "time")):
-            from builtin_interfaces.msg import Duration
-            val = float(data.get(get_namespace, 0.0))
-            frac, integral = math.modf(val)
-            sec = int(integral)
-            nanosec = round(frac * 1_000_000_000)
-            if nanosec >= 1_000_000_000:
-                sec += 1
-                nanosec -= 1_000_000_000
-            value = Duration(sec=sec, nanosec=nanosec)
-        elif ((msg_type == "deg") or (msg_type == "degree")):
-            value = float(data.get(get_namespace, 0.0)) * math.pi / 180.0
-        else:
-            pass
-            # TODO: List of (int or float or str)
-        
-        if (value is not None):
-            if (mem_dict[-1] == ""):
-                msg = value
-            else:
-                setattr(target, mem_dict[-1], value)
+                if (i == len(mem_dict) - 1):
+                    setattr(target, attr, value)
+                target = getattr(target, attr)
 
     return msg
 
---
+++ blockly_ros2/static/custom_blocks/joint_controllerjoint_trajectory.js
@@ -0,0 +1,200 @@
+import {
+  customExecutors
+} from "../core/registry.js";
+
+import { highlight }
+  from "../core/utils.js";
+
+
+
+// =========================
+// 定義
+// =========================
+
+Blockly.Blocks['send_joint_controllerjoint_trajectory'] = {
+
+  init: function () {
+
+    this.appendDummyInput()
+      ;
+    this.appendDummyInput()
+      .appendField("ジョイント名")
+      .appendField(
+        
+        
+        new Blockly.FieldTextInput(""),
+        
+        
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__0__"
+      )
+      .appendField(
+        
+        
+        new Blockly.FieldTextInput(""),
+        
+        
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__1__"
+      )
+      .appendField(
+        
+        
+        new Blockly.FieldTextInput(""),
+        
+        
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__2__"
+      )
+      ;
+    this.appendDummyInput();
+    this.appendDummyInput()
+      .appendField("角度")
+      .appendField(
+        
+        
+        
+        
+        new Blockly.FieldNumber(0, -Infinity, Infinity, 1),
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__0__"
+      )
+      .appendField(
+        
+        
+        
+        
+        new Blockly.FieldNumber(0, -Infinity, Infinity, 1),
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__1__"
+      )
+      .appendField(
+        
+        
+        
+        
+        new Blockly.FieldNumber(0, -Infinity, Infinity, 1),
+        
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__2__"
+      )
+      ;
+    this.appendDummyInput()
+      .appendField("移動時間")
+      .appendField(
+        
+        
+        
+        
+        
+        new Blockly.FieldNumber(0.0, 0.0, Infinity, 0.001),
+        "JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____TIME_FROM_START"
+      )
+      ;
+
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+
+    this.setColour(230);
+  }
+};
+
+
+
+// =========================
+// Python送信
+// =========================
+
+async function sendJointControllerjointTrajectory(block) {
+
+  await fetch("/joint_controllerjoint_trajectory", {
+
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    body: JSON.stringify({
+      
+      joint_controllerjoint_trajectory___joint_names__0__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__0__"),
+      
+      joint_controllerjoint_trajectory___joint_names__1__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__1__"),
+      
+      joint_controllerjoint_trajectory___joint_names__2__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__2__"),
+      
+      joint_controllerjoint_trajectory___points__0____positions__0__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__0__"),
+      
+      joint_controllerjoint_trajectory___points__0____positions__1__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__1__"),
+      
+      joint_controllerjoint_trajectory___points__0____positions__2__: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__2__"),
+      
+      joint_controllerjoint_trajectory___points__0____time_from_start: block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____TIME_FROM_START"),
+      
+    })
+  });
+}
+
+
+
+// =========================
+// 実行処理
+// =========================
+
+async function executeJointControllerjointTrajectory(block) {
+
+  const oldColor =
+    await highlight(block);
+
+  
+  const joint_controllerjoint_trajectory___joint_names__0__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__0__");
+  
+  const joint_controllerjoint_trajectory___joint_names__1__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__1__");
+  
+  const joint_controllerjoint_trajectory___joint_names__2__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___JOINT_NAMES__2__");
+  
+  const joint_controllerjoint_trajectory___points__0____positions__0__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__0__");
+  
+  const joint_controllerjoint_trajectory___points__0____positions__1__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__1__");
+  
+  const joint_controllerjoint_trajectory___points__0____positions__2__ =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____POSITIONS__2__");
+  
+  const joint_controllerjoint_trajectory___points__0____time_from_start =
+    block.getFieldValue("JOINT_CONTROLLERJOINT_TRAJECTORY___POINTS__0____TIME_FROM_START");
+  
+
+  
+  console.log(joint_controllerjoint_trajectory___joint_names__0__);
+  
+  console.log(joint_controllerjoint_trajectory___joint_names__1__);
+  
+  console.log(joint_controllerjoint_trajectory___joint_names__2__);
+  
+  console.log(joint_controllerjoint_trajectory___points__0____positions__0__);
+  
+  console.log(joint_controllerjoint_trajectory___points__0____positions__1__);
+  
+  console.log(joint_controllerjoint_trajectory___points__0____positions__2__);
+  
+  console.log(joint_controllerjoint_trajectory___points__0____time_from_start);
+  
+
+  await sendJointControllerjointTrajectory(block);
+
+  block.setColour(oldColor);
+
+}
+
+
+
+// =========================
+// 登録
+// =========================
+
+customExecutors["send_joint_controllerjoint_trajectory"] =
+  executeJointControllerjointTrajectory;
